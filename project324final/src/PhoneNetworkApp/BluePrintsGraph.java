5ef8e83ea5e4b5e3085d87579a1af8e5fd9ae83e
rabbitRM <102517158+rabbitRM@users.noreply.github.com>
2023-06-04 20:13:36 +0300

    Update BluePrintsGraph.java

--- project324final/src/PhoneNetworkApp/BluePrintsGraph.java
+++ project324final/src/PhoneNetworkApp/BluePrintsGraph.java
@@ -1,17 +1,8 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package PhoneNetworkApp;
 
 import GraphFramework.Graph;
 import GraphFramework.Vertex;
 
-/**
- *
- * @author cvs
- */
 public class BluePrintsGraph extends Graph {
      
      // Defining the overriden method to create Line in the application framework instead of creating an edge
