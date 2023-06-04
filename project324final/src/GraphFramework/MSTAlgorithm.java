0db99186116bf7f26378e50d21058a77cbf3956a
rabbitRM <102517158+rabbitRM@users.noreply.github.com>
2023-06-04 20:33:02 +0300

    Update MSTAlgorithm.java

--- project324final/src/GraphFramework/MSTAlgorithm.java
+++ project324final/src/GraphFramework/MSTAlgorithm.java
@@ -8,6 +8,24 @@ public abstract class MSTAlgorithm {
     protected ArrayList<GraphFramework.Edge> MSTResultList = new ArrayList<GraphFramework.Edge>();
     protected static Graph graph;
 
+    //-----------------------------------------------------------------------------
+    // Defining the setters and getters
+    public ArrayList<Edge> getMSTResultList() {
+        return MSTResultList;
+    }
+
+    public void setMSTResultList(ArrayList<Edge> MSTResultList) {
+        this.MSTResultList = MSTResultList;
+    }
+
+    public static Graph getGraph() {
+        return graph;
+    }
+
+    public static void setGraph(Graph graph) {
+        MSTAlgorithm.graph = graph;
+    }
+
     
     //-----------------------------------------------------------------------------
     // Defining the method that should be overriden in the prim and kruskal algorithms classes
